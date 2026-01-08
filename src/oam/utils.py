dad068703ee36129dfaa18d7654f3fde411b5a26
Julien OPPLIGER <account@oppliger.cc>
2026-01-08 22:04:49 +0100

    Fix typing

--- src/oam/utils.py
+++ src/oam/utils.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from dataclasses import fields
 from dataclasses import is_dataclass
 import inspect
@@ -5,8 +6,11 @@ from typing import cast
 from typing import Type, TypeVar, Mapping, Any
 from oam.oam_object import OAMObject
 import oam
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from _typeshed import DataclassInstance
 
-def _get_oam_obj_by_name(name: str) -> type[OAMObject]:
+def _get_oam_obj_by_name(name: str) -> Type[OAMObject]:
     for [_name, cls] in inspect.getmembers(oam, inspect.isclass):
         if _name == name:
             return cast(type[OAMObject], cls)
@@ -16,17 +20,17 @@ def _get_oam_obj_by_name(name: str) -> type[OAMObject]:
 def get_property_by_type(type: oam.PropertyType) -> Type[oam.Property]:
     if type not in oam.PropertyList:
         raise Exception("unsupported relation type")
-    return cast(oam.Property, _get_oam_obj_by_name(type.value))
+    return cast(Type[oam.Property], _get_oam_obj_by_name(type.value))
 
 def get_relation_by_type(type: oam.RelationType) -> Type[oam.Relation]:
     if type not in oam.RelationList:
         raise Exception("unsupported relation type")
-    return cast(oam.Relation, _get_oam_obj_by_name(type.value))
+    return cast(Type[oam.Relation], _get_oam_obj_by_name(type.value))
 
 def get_asset_by_type(type: oam.AssetType) -> Type[oam.Asset]:
     if type not in oam.AssetList:
         raise Exception("unsupported asset type")
-    return cast(oam.Asset, _get_oam_obj_by_name(type.value))
+    return cast(Type[oam.Asset], _get_oam_obj_by_name(type.value))
 
 def describe_oam_object(o: Type[OAMObject]) -> list:
     d = []
@@ -40,7 +44,7 @@ T = TypeVar("T")
 
 def make_oam_object_from_dict(o: Type[T], d: Mapping[str, Any]) -> T:
     real_d = {}
-    o_fields = fields(o)
+    o_fields = fields(cast(Any, o))
     for key, value in d.items():
         for field in o_fields:
             if ("json" in field.metadata and field.metadata["json"] == key) \
